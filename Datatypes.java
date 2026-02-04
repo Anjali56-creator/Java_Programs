66d07ff70d42c6d49f5ce2b4c967c6852666c5fb
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2026-02-04 13:02:10 +0530

    java

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes {
+    public static void main(String[] args) {
+       int a=10;
+       long l=58462l;
+       float f=5.5f;
+       double d=20.99;
+       char c='A';
+       boolean b=true;
+       System.out.println(a);
+         System.out.println(l);
+           System.out.println(f);
+             System.out.println(d);
+               System.out.println(c);
+                 System.out.println(b);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+
+    public static void main(String[] args) {
+        int a=5;
+        int b=20;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(b%a);
+    }
+}
+
