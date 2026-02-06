bea546aa4ae1123b7cb6b253d5f5e0977e71212e
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2026-02-06 15:59:39 +0530

    literals

---
+++ Basic/literals.java
@@ -0,0 +1,15 @@
+package Basic;
+public class literals {
+public static void main(String[] args) {
+    //literals
+    int n=0b1101; //binary literal
+    System.out.println(n);
+    int num1=0x1A; //hexadecimal literal
+    System.out.println(num1);
+    int n1=10_00_000; //using underscore in numeric literal
+    System.out.println(n1);
+    char c='a';
+    c++;
+    System.out.println(c);
+}
+}
