7ef9d445b0adad252ee01cfefa6318770af41183
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2026-02-07 10:01:41 +0530

    conversion

---
+++ Basic/conversion.java
@@ -0,0 +1,13 @@
+package Basic;
+class conversion{
+    public static void main(String[] args)
+    {
+        byte b=127;
+        int a=(byte)b;
+        System.out.println(a);
+
+        float f=3.14f;
+        int i=(int)f;
+        System.out.println(i);
+    }
+}
