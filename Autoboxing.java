599e99dc48dd05c217f35c4c8bb8b84e5c65801f
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2026-03-25 11:38:22 +0530

    autoboxing

---
+++ Autoboxing.java
@@ -0,0 +1,15 @@
+public class Autoboxing {
+    public static void main(String[] args) {
+        int x=10;
+        Integer y=x; //autoboxing
+        System.out.println(x);
+        System.out.println(y);//unboxing
+        Integer a=20;//autoboxing
+        int b=a;//unboxing
+        System.out.println(a);
+        System.out.println(b);
+    }
+    static void printinteger(int x){
+        System.out.println(x);
+    }
+}
