2a2bbea3bcd8c0e57ee24d7c2002ec4a0bbe2469
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-03-28 12:06:48 +0530

    anonymous_class

---
+++ Nested class/local_class.java
@@ -0,0 +1,24 @@
+public class local_class {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.greet();
+    }
+}
+
+// Effective final variable
+
+class Outer {
+    void greet() {
+        int y = 5;
+        // y++;
+
+        class Local {
+            void sayHello() {
+                System.out.println(y);
+            }
+        }
+
+        Local local  = new Local();
+        local.sayHello();
+    }
+}
