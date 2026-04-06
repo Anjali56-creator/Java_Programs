e54d7505e12113b5bdfda810012efd4e63ba801c
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-04-06 12:40:24 +0530

    string

--- string/basic.java
+++ string/basic.java
@@ -2,9 +2,12 @@ package string;
 
 public class basic {
     public static void main(String[] args) {
-        String name="Anjali";
-        String name2="kumari";
-        System.out.println(name+" "+name2);
+       
+         String s1="Hello";
+         String s2="Hello";
+         System.out.println(s1==s2); // true because both s1 and s2 refer to the same string literal in the string pool
+         String s3=new String("World");
+         String s4=new String("World");
+         System.out.println(s3==s4); // false because s3 and s4 refer to different objects in the heap
     }
-    
 }
